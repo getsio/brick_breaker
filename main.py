7c9f20ebe55cc3ce8d3b2e62c8ee42b38f55ef35
Aduronir <georgios.tsiokas@gmail.com>
2021-10-02 22:46:14 +0200

    Struktur erstellt

---
+++ main.py
@@ -0,0 +1,5 @@
+import pygame
+import pygame.display
+import pygame.draw
+import pygame.time
+from abc import ABCMeta, abstractmethod
---
+++ src/character.py
@@ -0,0 +1,13 @@
+import pygame
+import pygame.display
+import pygame.draw
+import pygame.time
+from abc import ABCMeta, abstractmethod
+
+class GameCharacter:
+  def __init__(self):
+    self.x = 0
+    self.y = 0
+    # Clockwise: 0 = left, 1 = right
+    self.direction = [False, False]
+    self.color = [255, 255, 255]
